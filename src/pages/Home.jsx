f02902198149005872542e49cfa12035b1f7c9f1
jay0425 <heedev94@gmail.com>
2023-09-05 14:29:52 +0900

    Feat : 메인페이지 추천콘텐츠

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
+import Recommend from './Recommend';
+
 function Home() {
   return (
     <div>
       <h2 className="text-slate-400">홈</h2>
+      <Recommend />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
