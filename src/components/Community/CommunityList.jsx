2c34ba8c7b9b5f450aa8ad036eb3002f5cfdca9e
jay0425 <heedev94@gmail.com>
2023-09-13 11:46:42 +0900

    Feat : 커뮤니티 리스트 UI 구현

---
+++ src/components/Community/CommunityList.jsx
@@ -0,0 +1,30 @@
+import Pagination from '../Pagination';
+import CommunityListPost from './CommunityListPost';
+import CommunityListProcedure from './CommunityListProcedure';
+
+function CommunityList() {
+  return (
+    <div className="flex justify-center">
+      <div>
+        <div className="CommunityListContainer w-[1015px] flex-col justify-center items-center py-[40px]">
+          <h1 className="CommunityListSubject text-center text-black text-[28px] font-[700] mb-[50px]">
+            커뮤니티
+          </h1>
+          <CommunityListProcedure />
+          <CommunityListPost />
+          <CommunityListPost />
+          <div className="CommunityListButton flex justify-end mt-[12px]">
+            <button className="w-[140px] h-[52px] bg-primary text-white text-[16px] font-[500] rounded-10">
+              글쓰기
+            </button>
+          </div>
+        </div>
+        <div className="flex justify-center py-[16px]">
+          <Pagination />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CommunityList;
