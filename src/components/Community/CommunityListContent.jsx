77461fa33ecf21f98338aa16ed786be509fae0d8
jay0425 <heedev94@gmail.com>
2023-09-19 14:25:40 +0900

    Fix : CommunityListContent 랑 CommunityListInfo 합치기

--- src/components/Community/CommunityListContent.jsx
+++ src/components/Community/CommunityListContent.jsx
@@ -22,6 +22,25 @@ function CommunityListContent({ item }) {
             </div>
           </div>
         </Link>
+
+        <div className="flex items-center">
+          <div className="flex">
+            <img
+              src={getPbImageURL(item.expand.user, 'profile')}
+              alt="프로필 사진"
+              className="text-[12px] w-[30px] h-[30px] text-black font-[400] rounded-[50%]"
+            />
+            <div className="flex items-center text-[12px] text-black font-[400] ml-[8px]">
+              {item?.expand?.user.nickname}
+            </div>
+          </div>
+          <div className="ml-[44px] text-[12px] text-black font-[400]">
+            댓글: {item?.comment.length}
+          </div>
+          <div className="ml-[44px] text-[12px] text-black font-[400]">
+            {item.updated}
+          </div>
+        </div>
       </>
     );
   }
--- src/components/Community/CommunityListPost.jsx
+++ src/components/Community/CommunityListPost.jsx
@@ -1,5 +1,4 @@
 import CommunityListContent from './CommunityListContent';
-import CommunityListInfo from './CommunityListInfo';
 import { useState, useEffect } from 'react';
 import pb from '@/api/pocketbase';
 
@@ -26,11 +25,34 @@ function CommunityListPost() {
 
   return (
     <>
+      <fieldset className="CommunityListProcedure flex">
+        <div className="" onClick="">
+          <input
+            id="radio1"
+            type="radio"
+            name="radio"
+            className="hidden"
+            checked
+          />
+          <label htmlFor="radio1" className="flex items-center cursor-pointer">
+            <span className="w-4 h-4 inline-block mr-1 rounded-full border border-grey ml-[20px]"></span>
+            최신순
+          </label>
+        </div>
+
+        <div className="" onClick="">
+          <input id="radio2" type="radio" name="radio" className="hidden" />
+          <label htmlFor="radio2" className="flex items-center cursor-pointer">
+            <span className="w-4 h-4 inline-block mr-1 rounded-full border border-grey ml-[20px]"></span>
+            인기순
+          </label>
+        </div>
+      </fieldset>
+
       {data?.items?.map((item) => (
         <div key={item.id}>
           <div className="border-b-2 border-[#747474] p-[20px]">
             <CommunityListContent key={item.id} item={item} />
-            <CommunityListInfo key={item.id} item={item} />
           </div>
         </div>
       ))}
