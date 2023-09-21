db32f5418b53dc2753051cc06ecafc930132fea8
jay0425 <heedev94@gmail.com>
2023-09-21 19:29:10 +0900

    Fix : 최신순, 인기순 기능 구현

--- src/components/Community/CommunityListPost.jsx
+++ src/components/Community/CommunityListPost.jsx
@@ -3,77 +3,40 @@ import { useState, useEffect } from 'react';
 import pb from '@/api/pocketbase';
 
 function CommunityListPost() {
+  const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState([]);
   const [check, setCheck] = useState('-created');
 
   useEffect(() => {
     async function getCommunity() {
+      setIsLoading(true);
       try {
         const record = await pb.collection('community').getList(1, 30, {
           sort: check,
           expand: 'user',
         });
         setData(record);
+        setIsLoading(false);
       } catch (error) {
         if (!(error in DOMException)) {
           console.error();
+          setIsLoading(false);
         }
       }
     }
 
     getCommunity();
-  }, []);
-
-  // function CheckCreated() {
-  //   useEffect(() => {
-  //     setCheck('-created');
-  //   }, []);
-  // }
-
-  // function CheckedLike() {
-  //   setCheck('-like');
-  // }
-
-  // ------------------------------------------------------------------
-
-  // const handleSortChange = (event) => {
-  //   setCheck(event.target.value);
-  // };
+  }, [check]);
 
   return (
     <>
-      {/* <div className="sort-container">
-        <label>
-          <input
-            type="radio"
-            name="sort"
-            value="-creadted"
-            checked={check === '-created'}
-            onChange={(e) => {
-              setCheck(e.target.value);
-            }}
-          />
-          최신순
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="sort"
-            value="-like"
-            checked={check === '-like'}
-            onChange={handleSortChange}
-          />
-          인기순
-        </label>
-      </div> */}
-
       <fieldset className="CommunityListProcedure flex">
         <input
           id="radio1"
           type="radio"
           name="radio"
           className="hidden"
-          checked
+          checked={check === '-created'}
           onClick={() => setCheck('-created')}
         />
         <label htmlFor="radio1" className="flex items-center cursor-pointer">
@@ -86,6 +49,7 @@ function CommunityListPost() {
           type="radio"
           name="radio"
           className="hidden"
+          checked={check === '-like'}
           onClick={() => setCheck('-like')}
         />
         <label htmlFor="radio2" className="flex items-center cursor-pointer">
@@ -94,13 +58,16 @@ function CommunityListPost() {
         </label>
       </fieldset>
 
-      {data?.items?.map((item) => (
-        <div key={item.id}>
-          <div className="border-b-2 border-[#747474] p-[20px]">
-            <CommunityListContent item={item} />
+      {isLoading && <p className="p-10 text-center">로딩 중...</p>}
+
+      {!isLoading &&
+        data?.items?.map((item) => (
+          <div key={item.id}>
+            <div className="border-b-2 border-[#747474] p-[20px]">
+              <CommunityListContent item={item} />
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </>
   );
 }
